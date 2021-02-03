9c0b15c06acd9cf8267f80ec1455160b973e6207
alber <alber_liu@qq.com>
2021-02-03 20:15:46 +0800

    日志优化

--- lib/net/api.dart
+++ lib/net/api.dart
@@ -5,21 +5,6 @@ import 'package:grpc/grpc.dart';
 const String baseUrl = "47.242.224.6";
 const String uploadUrl = "http://47.242.224.6:8085/upload";
 
-var interceptors = Interceptor();
-
-class Interceptor implements ClientInterceptor {
-  ResponseStream interceptStreaming(ClientMethod method,
-      Stream requests, CallOptions options,
-      ClientStreamingInvoker invoker) {
-    return invoker.call(method, requests, options);
-  }
-
-  ResponseFuture<R> interceptUnary(ClientMethod<Q, R> method, Q request,
-      CallOptions options, ClientUnaryInvoker<Q, R> invoker) {
-
-  }
-}
-
 final logicClient = LogicExtClient(ClientChannel(
   baseUrl,
   port: 50001,
